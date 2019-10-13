d61aaaf2f04b2d37c77e04e6f9a1314f39212066
Cynthia Revstrom <me@cynthia.re>
2019-10-13 19:57:07 +0200

    add templates

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.7
 RUN mkdir /app
 WORKDIR /app
 ADD cyntest /app/
+ADD src/templates /app/templates
 RUN chmod +x /app/cyntest
 
 EXPOSE 5000
--- src/main.go
+++ src/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/gorilla/mux"
 )
 
 var buildVersion string
 var buildTime string
 
+type indexPageData struct {
+	Hostname string
+}
+
 func main() {
-	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+	r := mux.NewRouter()
+	hostname, _ := os.Hostname()
+
+	indexTmpl := template.Must(template.ParseFiles("templates/index.tmpl.html"))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := indexPageData{
+			Hostname: hostname,
+		}
+		indexTmpl.Execute(w, data)
+	})
+
+	r.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "version: %s\nbuild_time: %s", buildVersion, buildTime)
 	})
-	http.ListenAndServe(":5000", nil)
+
+	http.ListenAndServe(":5000", r)
 }
