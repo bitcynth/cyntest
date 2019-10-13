fe54282ff8974f64d3526f74199c1f7e9b47a299
Cynthia Revstrom <me@cynthia.re>
2019-10-13 14:26:15 +0200

    add /node

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return f'Hello from {socket.gethostname()}'
 
+@app.route('/node')
+def node():
+    return socket.gethostname()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
